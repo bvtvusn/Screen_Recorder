662a5483fc5e7f8386750a7029fd1c7499592428
bvtvusn <57758767+bvtvusn@users.noreply.github.com>
2022-04-04 22:54:03 +0200

    Add files via upload

---
+++ ScreenCapture/ScreenCapture/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ScreenCapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStartRecording = new System.Windows.Forms.Button();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblDimensions = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtExtension = new System.Windows.Forms.TextBox();
+            this.chkIncludeCursor = new System.Windows.Forms.CheckBox();
+            this.btnRunFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStartRecording
+            // 
+            this.btnStartRecording.Location = new System.Drawing.Point(12, 38);
+            this.btnStartRecording.Name = "btnStartRecording";
+            this.btnStartRecording.Size = new System.Drawing.Size(98, 23);
+            this.btnStartRecording.TabIndex = 0;
+            this.btnStartRecording.Text = "Start Recording";
+            this.btnStartRecording.UseVisualStyleBackColor = true;
+            this.btnStartRecording.Click += new System.EventHandler(this.btnStartRecording_Click);
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Location = new System.Drawing.Point(116, 40);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.Size = new System.Drawing.Size(187, 20);
+            this.txtFilename.TabIndex = 1;
+            this.txtFilename.Text = "filename";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(324, 189);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblDimensions
+            // 
+            this.lblDimensions.AutoSize = true;
+            this.lblDimensions.Location = new System.Drawing.Point(13, 282);
+            this.lblDimensions.Name = "lblDimensions";
+            this.lblDimensions.Size = new System.Drawing.Size(61, 13);
+            this.lblDimensions.TabIndex = 4;
+            this.lblDimensions.Text = "Dimensions";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(326, 20);
+            this.txtPath.TabIndex = 5;
+            this.txtPath.Text = "path";
+            // 
+            // txtExtension
+            // 
+            this.txtExtension.Location = new System.Drawing.Point(309, 41);
+            this.txtExtension.Name = "txtExtension";
+            this.txtExtension.Size = new System.Drawing.Size(29, 20);
+            this.txtExtension.TabIndex = 6;
+            this.txtExtension.Text = "mp4";
+            // 
+            // chkIncludeCursor
+            // 
+            this.chkIncludeCursor.AutoSize = true;
+            this.chkIncludeCursor.Checked = true;
+            this.chkIncludeCursor.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkIncludeCursor.Location = new System.Drawing.Point(16, 67);
+            this.chkIncludeCursor.Name = "chkIncludeCursor";
+            this.chkIncludeCursor.Size = new System.Drawing.Size(94, 17);
+            this.chkIncludeCursor.TabIndex = 7;
+            this.chkIncludeCursor.Text = "Include Cursor";
+            this.chkIncludeCursor.UseVisualStyleBackColor = true;
+            // 
+            // btnRunFile
+            // 
+            this.btnRunFile.Location = new System.Drawing.Point(240, 63);
+            this.btnRunFile.Name = "btnRunFile";
+            this.btnRunFile.Size = new System.Drawing.Size(98, 23);
+            this.btnRunFile.TabIndex = 8;
+            this.btnRunFile.Text = "Play video file";
+            this.btnRunFile.UseVisualStyleBackColor = true;
+            this.btnRunFile.Click += new System.EventHandler(this.btnRunFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 303);
+            this.Controls.Add(this.btnRunFile);
+            this.Controls.Add(this.chkIncludeCursor);
+            this.Controls.Add(this.txtExtension);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lblDimensions);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.btnStartRecording);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStartRecording;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblDimensions;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtExtension;
+        private System.Windows.Forms.CheckBox chkIncludeCursor;
+        private System.Windows.Forms.Button btnRunFile;
+    }
+}
+
---
+++ ScreenCapture/ScreenCapture/Form1.cs
@@ -0,0 +1,136 @@
+﻿using Accord.Video.FFMPEG;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenCapture
+{
+    public partial class Form1 : Form
+    {
+        VideoFileWriter vf;
+        Rectangle screenSize;
+        public Form1()
+        {
+            InitializeComponent();
+            screenSize = GetSize();
+            lblDimensions.Text = "width: " + screenSize.Width + " Height: " + screenSize.Height;
+            btnStartRecording.Text = "Set new filepath";
+        }
+
+        private Rectangle GetSize()
+        {
+            //return Screen.FromControl(this).Bounds;
+            //return new Rectangle(0, 0, 600, 600);
+            return Screen.FromControl(this).WorkingArea;
+        }
+
+        private string GetSavePath()
+        {
+            return txtPath.Text + @"\"+ txtFilename.Text + "." + txtExtension.Text;
+        }
+        private void btnStartRecording_Click(object sender, EventArgs e)
+        {
+            string savepath;
+
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                vf.Close();
+                btnStartRecording.Text = "Start recording";
+            }
+            else
+            {
+                try
+                {
+                    if (txtPath.Text == "") throw new InvalidOperationException();
+                    savepath = Path.GetFullPath(GetSavePath());
+                    if (File.Exists(savepath)) throw new FileLoadException();
+
+                    screenSize = GetSize();
+                    vf = new VideoFileWriter();
+                    vf.Open(savepath, screenSize.Width, screenSize.Height, 25, VideoCodec.H264, 4000000);
+                    timer1.Start();
+                    btnStartRecording.Text = "Stop recording";
+                }
+                catch (Exception)
+                {
+                    saveFileDialog1 = new SaveFileDialog();
+                    saveFileDialog1.Filter = "Video (*.mp4)|*.mp4";
+                    saveFileDialog1.DefaultExt = "mp4";
+                    saveFileDialog1.AddExtension = true;
+                    saveFileDialog1.FileName = DateTime.Now.ToString("yyyy.MM.dd_hh.mm.ss");
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        string sourceFile = saveFileDialog1.FileName.ToString();
+                        txtExtension.Text = Path.GetExtension(sourceFile);
+                        txtFilename.Text = Path.GetFileNameWithoutExtension(sourceFile);
+                        txtPath.Text = Path.GetDirectoryName(sourceFile);
+
+                    }
+                    btnStartRecording.Text = "Start recording";
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Bitmap bp = new Bitmap(screenSize.Width, screenSize.Height);
+            Graphics gr = Graphics.FromImage(bp);
+            gr.CopyFromScreen(0, 0, 0, 0, new Size(bp.Width, bp.Height));
+
+            if (chkIncludeCursor.Checked)
+            {
+                Rectangle cursor = new Rectangle(GetMousePosition(), new Size(10, 10));
+                System.Windows.Forms.Cursor.Current.Draw(gr, cursor);
+            }
+            
+            pictureBox1.Image = bp;
+            vf.WriteVideoFrame(bp);
+        }
+
+
+        #region GetGlobalMousePosition
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        public static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+
+
+        #endregion
+
+        private void btnRunFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(GetSavePath());
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+    }
+}
